703a53550f3100042cc328edab79aa5dbcbe4dab
Manitej Narayanadasu <manitej2425@gmail.com>
2026-04-29 15:53:54 +0530

    added score function

--- main.py
+++ main.py
@@ -4,23 +4,28 @@ is_upcase = False
 is_lwcase = False
 is_special = False
 is_digit = False
+score = 0
 
 ##Now, Check for the strength of the password
 #break down each character of the password and evaluate
 for char in password:
     if char.isupper():
         is_upcase = True
+        score += 1
     elif char.islower():
         is_lwcase = True
+        score += 1
     elif char.isdigit():
         is_digit = True
+        score += 1
     elif char in "!@#$%^&*()_+-=?/.,<>?":
         is_special = True
+        score += 1
 #Cases for password strength
 if is_upcase and is_lwcase and is_special and is_digit:
-    print("Strong password")
+    print("Strong password", score)
 if is_upcase and is_lwcase and is_special==False and is_digit==False:
-    print("Try a better password")
+    print("Try a better password", score)
 if is_upcase==False and is_special==False:
-    print("Weak password")
+    print("Weak password", score)
 
